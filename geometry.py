81636a5e5dfdaede36242051956a67c82b2a998d
wildk <wildkratts555@gmail.com>
2025-06-25 18:51:06 +0530

    hi

--- geometry.py
+++ geometry.py
@@ -4,7 +4,6 @@ def gen_edges(points):
         edges.append(Edge(points[i], points[i+1]))
     return edges
 
-
 class Polygon:
     def __init__(self, points):
         self.points = points
@@ -15,6 +14,15 @@ class Polygon:
 
     def __getitem__(self, item):
         return self.points[item % len(self.points)]
+
+    def double_res(self):
+        new_points = []
+        for i in range(len(self.points)):
+            p1 = self[i]
+            p2 = self[(i + 1) % len(self.points)]
+            new_points.append(p1)
+            new_points.append(p1.get_mid(p2))
+        return Polygon(new_points)
 
 class Point:
     def __init__(self, x, y, z):
@@ -28,11 +36,14 @@ class Point:
     def __sub__(self, other):
         return Point(self.x - other.x, self.y - other.y, self.z - other.z)
 
-    def __mul__(self, other):
+    def __mul__(self, other:int|float):
         return Point(self.x * other, self.y * other, self.z * other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other:int|float):
         return Point(self.x / other, self.y / other, self.z / other)
+
+    def get_mid(self, other):
+        return Point((self.x + other.x) / 2, (self.y + other.y) / 2, (self.z + other.z) / 2)
 
 class Edge:
     def __init__(self, p1, p2):
@@ -45,3 +56,6 @@ class Face:
         self.p2 = p2
         self.p3 = p3
         self.edges = gen_edges([p1, p2, p3])
+
+    def double_res(self):
+        return Polygon([self.p1, self.p1.get_mid(self.p2), self.p2, self.p2.get_mid(self.p3), self.p3, self.p3.get_mid(self.p1)])
